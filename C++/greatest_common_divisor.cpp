1fbde4a316a6a32ede87a4830c8377c570d39bad
FasterCoding <fastercodingtutorial@gmail.com>
2019-05-08 23:43:41 +0200

    :sparkles: Add GCD Function

--- C++/greatest_common_divisor.cpp
+++ C++/greatest_common_divisor.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+int gcd(int a, int b) {
+
+    int result;
+
+    if (b == 0) {
+        return a;
+    } else if (a == 0) {
+        return b;
+    } else {
+
+        if (a > b) {
+            result = gcd(a - b, b);
+        } else {
+            result = gcd(a, b - a);
+        }
+
+    } 
+
+    return result;
+}
+
 int main(int argc, char *argv[]) {
+
+    int a, b;
+
+    std::cout << "Enter a: ";
+    std::cin >> a;
+
+    std::cout << "Enter b: ";
+    std::cin >> b;
+
+    int greatestCommonDivisor = gcd(a, b);
+
+    std::cout << "Greatest Common Divisor: " << greatestCommonDivisor << std::endl;
 
     return 0;
 }
