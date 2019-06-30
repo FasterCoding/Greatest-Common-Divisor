82ca7dd033b5343cbbf3bd297b673b318f483f97
FasterCoding <fastercodingtutorial@gmail.com>
2019-06-30 21:43:55 +0200

    :sparkles: Add Extgcd in Java

--- Java/Euclid/src/main/java/com/fastercoding/euclid/Euclid.java
+++ Java/Euclid/src/main/java/com/fastercoding/euclid/Euclid.java
@@ -1,5 +1,7 @@
 package com.fastercoding.euclid;
 
+import java.util.Collection;
+
 public class Euclid {
 
     private Euclid() {}
@@ -14,5 +16,32 @@ public class Euclid {
         }
 
         return a;
+    }
+
+    public static int[] extgcd(int a, int b) {
+        // Return x and y in form a*x + b*y
+        // x is the inverse of a
+        // y is the inverse if b
+        // There is only an inverse if gcd(a,b) == 1
+        if (a == 0) {
+            return new int[]{0, 1};
+        }else {
+            int[] result = extgcd(b % a, a);
+            int ab = (int)Math.floor(b / a);
+
+            result[0] = (result[0] - ab * result[1]) % b;
+            result[0] = mod(result[0], b);
+
+            return result;
+        }
+    }
+
+    private static int mod(int a, int b) {
+        return ((a%b)+b) % b;
+    }
+
+    public static int moduloInverse(int a, int b) {
+        int[] result = extgcd(a, b);
+        return result[0];
     }
 }
