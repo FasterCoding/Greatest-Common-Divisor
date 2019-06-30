96c292379f7e692dadbc0fb0cb41800e18165af7
FasterCoding <fastercodingtutorial@gmail.com>
2019-06-30 21:29:49 +0200

    :sparkles: Add Extgcd in TypeScript

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,13 +1,14 @@
 import {Euclid} from "./src/euclid";
 
-const a: number = 16;
-const b: number = 24;
+const a: number = 4;
+const b: number = 5;
 
-const gcd: number = Euclid.gcd(a, b);
-console.log("Greatest Common Divisor: ", gcd);
+const resultGCD: number = Euclid.gcd(a, b);
+console.log("Greatest Common Divisor: ", resultGCD);
 
-const c: number = 15;
-const d: number = 14;
-
-const gcd2: number = Euclid.gcd(c, d);
-console.log("Greatest Common Divisor: ", gcd2);
+if (resultGCD == 1) {
+    const inverse = Euclid.moduloInverse(a, b);
+    console.log("inverse of " + a + " mod " + b +" is: " + inverse);
+} else {
+    console.log("inverse of " + a + " mod " + b +" is: No Inverse");
+}
--- typescript/src/euclid.ts
+++ typescript/src/euclid.ts
@@ -1,3 +1,5 @@
+import {IExtEuclid} from "./iexteuclid";
+
 export class Euclid {
 
     static gcd(a: number, b: number): number {
@@ -9,6 +11,35 @@ export class Euclid {
         }
 
         return a;
+    }
+
+    static extgcd(a: number, b: number): IExtEuclid {
+        // Return x and y in form a*x + b*y
+        // x is the inverse of a
+        // y is the inverse if b
+        // There is only an inverse if gcd(a,b) == 1
+        if (a == 0) {
+            const result: IExtEuclid = {x: 0, y: 1};
+            return result;
+        }else { 
+
+            let result: IExtEuclid = this.extgcd(b % a, a);
+            const ab = Math.floor(b / a);
+
+            result.x = (result.x - ab * result.y);
+            result.x = this.mod(result.x, b);
+
+            return result;
+        }
+    }
+
+    private static mod(a: number, b: number) {
+        return ((a%b)+b) % b;
+    }
+
+    static moduloInverse(a: number, b: number): number {
+        const result: IExtEuclid = this.extgcd(a, b);
+        return result.x;
     }
 
 }
