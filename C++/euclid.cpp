e12376d1d2e2d9008b46532f74e659203f6a342d
FasterCoding <fastercodingtutorial@gmail.com>
2019-06-30 20:37:19 +0200

    :sparkles: Add Extgcd in C++

--- C++/euclid.cpp
+++ C++/euclid.cpp
@@ -11,3 +11,24 @@ int Euclid::gcd(int a, int b) {
 
     return a;
 }
+
+std::tuple<int, int> Euclid::extgcd(int a, int b) {
+    // Return in x and y form a*x + b*y
+    // x is the inverse of a
+    // y is the inverse if b
+    // There is only an inverse if gcd(a,b) == 1
+    if (a == 0) {
+        return std::make_tuple(0, 1);
+    }else {
+        std::tuple<int, int> result = extgcd(b % a, a);
+        int y = std::get<1>(result);
+        int x = (x - int(b / a) * y) % b;
+
+        return std::make_tuple(x, y);
+    }
+}
+
+int Euclid::moduloInverseOf(int a, int b) {
+    std::tuple<int, int> inv = extgcd(a, b);
+    return std::get<0>(inv);
+}
--- C++/euclid.hpp
+++ C++/euclid.hpp
@@ -1,6 +1,8 @@
 #ifndef __EUCLID_H_
 #define __EUCLID_H_
 
+#include <tuple>
+
 class Euclid {
 
 private:
@@ -10,7 +12,8 @@ private:
 public:
 
     static int gcd(int a, int b);
-
+    static std::tuple<int, int> extgcd(int a, int b);
+    static int moduloInverseOf(int a, int b);
 };
 
 #endif
