94e0b1b28b15c7b9fae2973e42cb112e51c80f3e
FasterCoding <fastercodingtutorial@gmail.com>
2019-07-02 12:41:58 +0200

    :bug: Fix TypeScript Ext Gcd

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,14 +1,23 @@
 import {Euclid} from "./src/Euclid";
 
-const a: number = 4;
+const numberList: Array<number> = [0, 1, 2, 3, 4, 5]; 
+
 const b: number = 5;
 
-const resultGCD: number = Euclid.gcd(a, b);
-console.log("Greatest Common Divisor: ", resultGCD);
+numberList.forEach(a => {
 
-if (resultGCD == 1) {
-    const inverse = Euclid.moduloInverse(a, b);
-    console.log("inverse of " + a + " mod " + b +" is: " + inverse);
-} else {
-    console.log("inverse of " + a + " mod " + b +" is: No Inverse");
-}
+    const resultGCD: number = Euclid.gcd(a, b);
+    console.log("Greatest Common Divisor: ", resultGCD);
+
+    if (resultGCD == 1) {
+        const inverse = Euclid.moduloInverse(a, b);
+        console.log("inverse of " + a + " mod " + b +" is: " + inverse);
+    } else {
+        console.log("inverse of " + a + " mod " + b +" is: No Inverse");
+    }
+
+    console.log("------------------------------");
+
+});
+
+
